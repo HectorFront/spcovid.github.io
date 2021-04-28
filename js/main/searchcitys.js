c84ea909d12c4573324aa48b7c17bbbf385e2170
Hector Silva <hectddos103@gmail.com>
2021-04-28 18:06:57 -0300

    otimizando arquivos

--- js/main/searchcitys.js
+++ js/main/searchcitys.js
@@ -1,1 +1,104 @@
-"use strict";function _toConsumableArray(a){if(Array.isArray(a)){for(var t=0,o=Array(a.length);t<a.length;t++)o[t]=a[t];return o}return Array.from(a)}var searchCity=function(){$("#message_view").html("");function t(a){return a.toLocaleString("pt-BR")}function o(a){var t,o=a,e={a:/[\xE0-\xE6]/g,e:/[\xE8-\xEB]/g,i:/[\xEC-\xEF]/g,o:/[\xF2-\xF6]/g,u:/[\xF9-\xFC]/g,c:/\xE7/g,n:/\xF1/g};for(t in e)var r=e[t],o=o.replace(r,t);return o}var e=null,r=$("#api_search").val(),l=$("#state_search").val();$("#list_cards").html(loadingCitys),$.get("https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalMunicipio").done(function(a){a.map(function(a){o(a.nome.toLowerCase())==o(r.toLowerCase().trim())&&a.cod.substr(0,2)==l&&(e=a.cod.substr(0,2),a="<div "+(1==localStorage.getItem("dark")?'style="background-color: #202020;"':'style="background-color: #FFFFFF"')+' class="card__covid" id="city_'+a.nome+'">\n                                <div class="card__title">\n                                    <h1 '+(1==localStorage.getItem("dark")?'style="color: rgb(240, 240, 240)"':'style="color: #000000"')+">"+a.nome+'&nbsp;<i style="color: #8498ae; font-size: 18px;" class="fas fa-map-marker-alt"></i></h1>\n                                </div>\n                                <div class="card__content">\n                                    <div class="card_column__info">\n                                        <h1 '+(1==localStorage.getItem("dark")?'style="color: rgb(240, 240, 240)"':'style="color: #000000"')+">"+t(Number(a.casosAcumulado))+"</h1>\n                                        <p "+(1==localStorage.getItem("dark")?'style="color: rgb(240, 240, 240)"':'style="color: #000000"')+'>Casos confirmados</p>\n                                    </div>\n                                    <div class="card_column__info">\n                                        <h1 '+(1==localStorage.getItem("dark")?'style="color: rgb(240, 240, 240)"':'style="color: #000000"')+">"+t(Number(a.obitosAcumulado))+"</h1>\n                                        <p "+(1==localStorage.getItem("dark")?'style="color: rgb(240, 240, 240)"':'style="color: #000000"')+'>Óbitos</p>\n                                    </div>\n                                    <div class="card_column__info">\n                                        <h1 '+(1==localStorage.getItem("dark")?'style="color: rgb(240, 240, 240)"':'style="color: #000000"')+' id="estadoCard"></h1>\n                                        <p '+(1==localStorage.getItem("dark")?'style="color: rgb(240, 240, 240)"':'style="color: #000000"')+">Estado</p>\n                                    </div>\n                                </div>\n                            </div>",$("#list_cards").html(a))}),setTimeout(function(){r&&$("#list_cards").html()==loadingCitys&&($("#list_cards").html('<p class="nenhumResultado">Cidade não disponível no momento...</p>'),setTimeout(function(){var a;r&&$("#list_cards").html()==$(".nenhumResultado")[0].outerHTML&&($(".nenhumResultado").remove(),$(".loading").remove(),$("#api_search").val(""),document.querySelector("#state_search").value=35,isStateAllSP?(a=paginate([].concat(_toConsumableArray(dataCovidCitysSP)),0,indexPagination+12),listDataRegion(a,!1)):renderCitys(dataCovidCitysSP,!0,function(a,t){$("#list_cards").append(a),$("#message_view").html('<h1 style="color: #8498ae; line-height: 25px; font-family: CardTitle; font-weight: 500; text-align: center; margin: 0 20px 0 20px;">Essas cidades são para a visualização padrão da página</h1>')}))},1500))},2e4),uf.map(function(a){return a.codigo_uf==e&&$("#estadoCard").html(a.nome)}),r||($(".loading").remove(),$("#list_cards").html(""),isStateAllSP?(a=paginate([].concat(_toConsumableArray(dataCovidCitysSP)),0,indexPagination+12),listDataRegion(a,!1)):renderCitys(dataCovidCitysSP,!0,function(a,t){$("#list_cards").append(a),$("#message_view").html('<h1 style="color: #8498ae; line-height: 25px; font-family: CardTitle; font-weight: 500; text-align: center; margin: 0 20px 0 20px;">Essas cidades são para a visualização padrão da página</h1>')}))})};
+const searchCity = () => {
+    $('#message_view').html('');
+
+    let codUF = null;
+    const inputCity = $("#api_search").val();
+    const selectState = $('#state_search').val();
+
+    const formatNumber = (value) => {
+        return value.toLocaleString("pt-BR");
+    };
+
+    const removerAcentos = (newStringComAcento) => {
+        let string = newStringComAcento;
+        let mapaAcentosHex = {
+            a: /[\xE0-\xE6]/g,
+            e: /[\xE8-\xEB]/g,
+            i: /[\xEC-\xEF]/g,
+            o: /[\xF2-\xF6]/g,
+            u: /[\xF9-\xFC]/g,
+            c: /\xE7/g,
+            n: /\xF1/g
+        };
+
+        for (let letra in mapaAcentosHex) {
+            let expressaoRegular = mapaAcentosHex[letra];
+            string = string.replace(expressaoRegular, letra);
+        }
+        return string;
+    }
+
+    $("#list_cards").html(loadingCitys);
+
+    $.get(
+        "https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalMunicipio"
+    ).done((covidata) => {
+
+        covidata.map((info) => {
+            if (removerAcentos(info.nome.toLowerCase()) == removerAcentos(inputCity.toLowerCase().trim()) && info.cod.substr(0, 2) == selectState) {
+                codUF = info.cod.substr(0, 2);
+                let cardCity =
+                    `<div ${localStorage.getItem('dark') == 1 ? 'style="background-color: #202020;"' : 'style="background-color: #FFFFFF"'} class="card__covid" id="city_${info.nome}">
+                                <div class="card__title">
+                                    <h1 ${localStorage.getItem('dark') == 1 ? 'style="color: rgb(240, 240, 240)"' : 'style="color: #000000"'}>${info.nome}&nbsp;<i style="color: #8498ae; font-size: 18px;" class="fas fa-map-marker-alt"></i></h1>
+                                </div>
+                                <div class="card__content">
+                                    <div class="card_column__info">
+                                        <h1 ${localStorage.getItem('dark') == 1 ? 'style="color: rgb(240, 240, 240)"' : 'style="color: #000000"'}>${formatNumber(Number(info.casosAcumulado))}</h1>
+                                        <p ${localStorage.getItem('dark') == 1 ? 'style="color: rgb(240, 240, 240)"' : 'style="color: #000000"'}>Casos confirmados</p>
+                                    </div>
+                                    <div class="card_column__info">
+                                        <h1 ${localStorage.getItem('dark') == 1 ? 'style="color: rgb(240, 240, 240)"' : 'style="color: #000000"'}>${formatNumber(Number(info.obitosAcumulado))}</h1>
+                                        <p ${localStorage.getItem('dark') == 1 ? 'style="color: rgb(240, 240, 240)"' : 'style="color: #000000"'}>Óbitos</p>
+                                    </div>
+                                    <div class="card_column__info">
+                                        <h1 ${localStorage.getItem('dark') == 1 ? 'style="color: rgb(240, 240, 240)"' : 'style="color: #000000"'} id="estadoCard"></h1>
+                                        <p ${localStorage.getItem('dark') == 1 ? 'style="color: rgb(240, 240, 240)"' : 'style="color: #000000"'}>Estado</p>
+                                    </div>
+                                </div>
+                            </div>`;
+
+                $("#list_cards").html(cardCity);
+            }
+        });
+
+        setTimeout(() => {
+            if(inputCity && $("#list_cards").html() == loadingCitys) {
+                let nenhumResultadoCount = `<p class="nenhumResultado">Cidade não disponível no momento...</p>`;
+                $('#list_cards').html(nenhumResultadoCount);
+                setTimeout(() => {
+                    if(inputCity && $("#list_cards").html() == $(".nenhumResultado")[0].outerHTML) {
+                        $('.nenhumResultado').remove();
+                        $(".loading").remove();
+                        $("#api_search").val('');
+                        document.querySelector('#state_search').value = 35;
+                        if (isStateAllSP) {
+                            let paginateCurrent = paginate([...dataCovidCitysSP], 0, indexPagination + 12);
+                            listDataRegion(paginateCurrent, false);
+                        } else {
+                            renderCitys(dataCovidCitysSP, true, (card, info) => {
+                                $("#list_cards").append(card);
+                                $('#message_view').html('<h1 style="color: #8498ae; line-height: 25px; font-family: CardTitle; font-weight: 500; text-align: center; margin: 0 20px 0 20px;">Essas cidades são para a visualização padrão da página</h1>');
+                            });
+                        }
+                    }
+                }, 1500)
+            }
+       }, 20000);
+
+        uf.map(resp => resp.codigo_uf == codUF && $('#estadoCard').html(resp.nome));
+        if (!inputCity) {
+            $(".loading").remove();
+            $("#list_cards").html('');
+            if (isStateAllSP) {
+                let paginateCurrent = paginate([...dataCovidCitysSP], 0, indexPagination + 12);
+                listDataRegion(paginateCurrent, false);
+            } else {
+                renderCitys(dataCovidCitysSP, true, (card, info) => {
+                    $("#list_cards").append(card);
+                    $('#message_view').html('<h1 style="color: #8498ae; line-height: 25px; font-family: CardTitle; font-weight: 500; text-align: center; margin: 0 20px 0 20px;">Essas cidades são para a visualização padrão da página</h1>');
+                });
+            }
+        }
+    });
+};
--- js/map/covidmap.js
+++ js/map/covidmap.js
@@ -1,1 +1,78 @@
-"use strict";var setViewMap=function(){function e(o){return o.toLocaleString("pt-BR")}$.get("https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalEstado").done(function(o){map=L.map("map",{minZoom:4,maxZoom:11,scrollWheelZoom:!1}).setView([-12.897489,-51.472334],4),map.zoomControl.setPosition("topright"),L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",{attribution:"COVID-19 no mapa do Brasil"}).addTo(map),L.control.scale().addTo(map),o.map(function(o){var n="SP"===o.nome&&ufLatLng.SP||"RJ"===o.nome&&ufLatLng.RJ||"CE"===o.nome&&ufLatLng.CE||"AM"===o.nome&&ufLatLng.AM||"PA"===o.nome&&ufLatLng.PA||"PE"===o.nome&&ufLatLng.PE||"MA"===o.nome&&ufLatLng.MA||"BA"===o.nome&&ufLatLng.BA||"ES"===o.nome&&ufLatLng.BA||"PB"===o.nome&&ufLatLng.PB||"MG"===o.nome&&ufLatLng.MG||"DF"===o.nome&&ufLatLng.DF||"RS"===o.nome&&ufLatLng.RS||"AP"===o.nome&&ufLatLng.AP||"AL"===o.nome&&ufLatLng.AL||"SC"===o.nome&&ufLatLng.SC||"SE"===o.nome&&ufLatLng.SE||"RN"===o.nome&&ufLatLng.RN||"AC"===o.nome&&ufLatLng.AC||"RO"===o.nome&&ufLatLng.RO||"PI"===o.nome&&ufLatLng.PI||"PR"===o.nome&&ufLatLng.PR||"TO"===o.nome&&ufLatLng.TO||"GO"===o.nome&&ufLatLng.GO||"RR"===o.nome&&ufLatLng.RR||"MT"===o.nome&&ufLatLng.MT||"MS"===o.nome&&ufLatLng.MS,t=L.icon({iconUrl:o.obitosAcumulado<100?"https://covidregional.github.io/images/markerLeve.png":900<o.obitosAcumulado?"https://covidregional.github.io/images/marker.png":"https://covidregional.github.io/images/markerMed.png",iconSize:[50,50]});L.marker(n,{icon:t}).addTo(map).bindPopup("<b style='font-weight: 900;'>Estado / "+o.nome+"</b>\n                        <p><b>Situação</b> "+(o.obitosAcumulado<100?"<span style='color: #009b74; font-weight: bold;'>LEVE</span>":100<o.obitosAcumulado&&"<span style='"+(900<o.obitosAcumulado?"color: #ff6262;":"color: #F5A700;")+" font-weight: bold;'>"+(900<o.obitosAcumulado?"CRÍTICA":"GRAVE")+"</span>")+"\n                        </p>\n                        <p>Confirmados: <b style='font-weight: 900;'>"+e(Number(o.casosAcumulado))+"</b></p>\n                        <p>Mortos: <b style='font-weight: 900;'>"+e(Number(o.obitosAcumulado))+"</b></p>\n                        <p>Incidência: <b style='font-weight: 900;'>"+o.incidencia+"%</b></p>\n                        <p>Incidência de óbitos: <b style='font-weight: 900;'>"+o.incidenciaObito+"%</b></p>\n                    ").on("mouseover",function(o){o.target.openPopup()})})})};setViewMap();
+const setViewMap = () => {
+
+    const formatNumber = (value) => {
+        return value.toLocaleString("pt-BR");
+    };
+    $.get('https://xx9p7hp1p7.execute-api.us-east-1.amazonaws.com/prod/PortalEstado')
+        .done(dataUf => {
+
+            map = L.map('map', { minZoom: 4, maxZoom: 11, scrollWheelZoom: false }).setView([-12.897489, -51.472334], 4);
+            map.zoomControl.setPosition('topright');
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: 'COVID-19 no mapa do Brasil',
+            }).addTo(map);
+
+            L.control.scale().addTo(map);
+
+            dataUf.map(info => {
+
+                let LatLngStates =
+                    info.nome === 'SP' && ufLatLng.SP ||
+                    info.nome === 'RJ' && ufLatLng.RJ ||
+                    info.nome === 'CE' && ufLatLng.CE ||
+                    info.nome === 'AM' && ufLatLng.AM ||
+                    info.nome === 'PA' && ufLatLng.PA ||
+                    info.nome === 'PE' && ufLatLng.PE ||
+                    info.nome === 'MA' && ufLatLng.MA ||
+                    info.nome === 'BA' && ufLatLng.BA ||
+                    info.nome === 'ES' && ufLatLng.BA ||
+                    info.nome === 'PB' && ufLatLng.PB ||
+                    info.nome === 'MG' && ufLatLng.MG ||
+                    info.nome === 'DF' && ufLatLng.DF ||
+                    info.nome === 'RS' && ufLatLng.RS ||
+                    info.nome === 'AP' && ufLatLng.AP ||
+                    info.nome === 'AL' && ufLatLng.AL ||
+                    info.nome === 'SC' && ufLatLng.SC ||
+                    info.nome === 'SE' && ufLatLng.SE ||
+                    info.nome === 'RN' && ufLatLng.RN ||
+                    info.nome === 'AC' && ufLatLng.AC ||
+                    info.nome === 'RO' && ufLatLng.RO ||
+                    info.nome === 'PI' && ufLatLng.PI ||
+                    info.nome === 'PR' && ufLatLng.PR ||
+                    info.nome === 'TO' && ufLatLng.TO ||
+                    info.nome === 'GO' && ufLatLng.GO ||
+                    info.nome === 'RR' && ufLatLng.RR ||
+                    info.nome === 'MT' && ufLatLng.MT ||
+                    info.nome === 'MS' && ufLatLng.MS;
+
+                let icon = L.icon({
+                    iconUrl:
+                    info.obitosAcumulado < 100
+                    ? 'https://covidregional.github.io/images/markerLeve.png'
+                    : info.obitosAcumulado > 900
+                    ?  'https://covidregional.github.io/images/marker.png'
+                    : 'https://covidregional.github.io/images/markerMed.png',
+                    iconSize: [50, 50],
+                });
+
+                let marker = L.marker(LatLngStates, { icon: icon }).addTo(map)
+                    .bindPopup(
+                    `<b style='font-weight: 900;'>Estado / ${info.nome}</b>
+                        <p><b>Situação</b> ${info.obitosAcumulado < 100 &&
+                            "<span style='color: #009b74; font-weight: bold;'>LEVE</span>" || info.obitosAcumulado > 100 &&
+                            `<span style='${info.obitosAcumulado > 900 ? 'color: #ff6262;' : 'color: #F5A700;'} font-weight: bold;'>${info.obitosAcumulado > 900 ? 'CRÍTICA' : 'GRAVE'}</span>`}
+                        </p>
+                        <p>Confirmados: <b style='font-weight: 900;'>${formatNumber(Number(info.casosAcumulado))}</b></p>
+                        <p>Mortos: <b style='font-weight: 900;'>${formatNumber(Number(info.obitosAcumulado))}</b></p>
+                        <p>Incidência: <b style='font-weight: 900;'>${info.incidencia}%</b></p>
+                        <p>Incidência de óbitos: <b style='font-weight: 900;'>${info.incidenciaObito}%</b></p>
+                    `);
+                marker.on('mouseover', function (ev) {
+                    ev.target.openPopup();
+                });
+            });
+        });
+};
+
+setViewMap();
