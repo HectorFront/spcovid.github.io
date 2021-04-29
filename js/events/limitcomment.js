6911c66fd3f93a550dd891bffa90dc4091e8b69c
Hector Silva <hectddos103@gmail.com>
2021-04-29 09:05:27 -0300

    minificando todos os arquivos HTML, CSS e JS

--- js/events/limitcomment.js
+++ js/events/limitcomment.js
@@ -1,13 +1,1 @@
-const limitComment = () =>{
-    const caracteres = $('.input__comment').val();
-    let countCaracteres = 500 - caracteres.length;
-    $('#qtd_caracteres').html(countCaracteres);
-    let qtd_span = $('#qtd_caracteres').html();
-    if(qtd_span == 0){
-        $('#qtd_caracteres').css('color', 'rgb(216, 60, 60)');
-        $('#qtd_caracteres').css('border', '5px solid rgb(216, 60, 60)');
-    }else{
-        $('#qtd_caracteres').css('color', '#1da584');
-        $('#qtd_caracteres').css('border', '5px solid #1da584');
-    }
-}
+"use strict";var limitComment=function(){var r=500-$(".input__comment").val().length;$("#qtd_caracteres").html(r),0==$("#qtd_caracteres").html()?($("#qtd_caracteres").css("color","rgb(216, 60, 60)"),$("#qtd_caracteres").css("border","5px solid rgb(216, 60, 60)")):($("#qtd_caracteres").css("color","#1da584"),$("#qtd_caracteres").css("border","5px solid #1da584"))};
